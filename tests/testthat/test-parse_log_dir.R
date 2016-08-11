16bf8e45ae4de2573522607949ac44fbe86973ea
Shantanu Singh <shsingh@broadinstitute.org>
2016-08-11 19:13:05 -0400

    Fixed tests

--- tests/testthat/test-parse_log_dir.R
+++ tests/testthat/test-parse_log_dir.R
@@ -1,13 +1,33 @@
 context("parse log")
 
-test_that("`parse_log` parses data", {
+test_that("`parse_log_dir` parses data", {
 
-  logfile <-
-      gzfile(system.file("extdata", "Plate_160519200001_Well_G07_000006.gz",
-                         package = "cplog")
-             )
+  log_dir_root <- system.file("extdata",
+                              "051816_3661_Q3Q4_subset/",
+                              package = "cplog")
 
-  logfile <- parse_log(logfile)
-  expect_equal(1, 1)
+  aws_logs <-
+    lapply(
+      list.dirs(log_dir_root, recursive = F, full.names = F),
+      function(log_dir) {
+        parse_log_dir(paste(log_dir_root, log_dir, sep = "/")) %>%
+          dplyr::mutate(log_dir = log_dir)
+      }
+    ) %>%
+    dplyr::bind_rows()
+
+  expect_equal(nrow(aws_logs), 3240)
+
+  aws_logs_expected <-
+  tibble::frame_data(
+    ~log_dir, ~n,
+    "Plate_160519200001_Well_A01", 810,
+    "Plate_160519200001_Well_A02", 810,
+    "Plate_160519200001_Well_A03", 810,
+    "Plate_160519200001_Well_A04", 810
+  )
+
+  expect_equal(aws_logs %>% dplyr::count(log_dir),
+               aws_logs_expected %>% dplyr::mutate(n = as.integer(n)))
 
 })
